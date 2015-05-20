90897941b8cae67dc38af98de5dad350f272dd1c
Jacob Albers <jacobleealbers@gmail.com>
2015-05-20 18:38:33 -0500

    updated with cabal files

---
+++ src/db.hs
@@ -0,0 +1,38 @@
+{-
+Jacob Albers
+CMSC 22311 - Functional Systems in Haskell
+Instructors: Stuart Kurtz & Jakub Tucholski
+-}
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Distributed.Process
+import Control.Monad.IO.Class
+import Control.Monad
+import System.IO
+
+import DistribUtils
+
+import Database  (Database, createDB, get, set, rcdata)
+
+main = distribMain master rcdata
+
+master :: [NodeId] -> Process ()
+master peers = do
+  db <- createDB peers
+
+  f <- liftIO $ readFile "Database.hs"
+  let ws = words f
+
+  zipWithM_ (set db) ws (tail ws)
+
+  get db "module" >>= liftIO . print
+  get db "xxxx"   >>= liftIO . print
+
+  forever $ do
+    l <- liftIO $ do putStr "key: "; hFlush stdout; getLine
+    when (not (null l)) $ do
+      r <- get db l
+      liftIO $ putStrLn ("response: " ++ show r)
+
+  return ()
