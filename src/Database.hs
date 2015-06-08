61781c198b818ad946449b65752b5c367827c201
Jacob Albers <jacobleealbers@gmail.com>
2015-06-08 12:02:19 -0500

    added distributive stuff

--- src/Database.hs
+++ src/Database.hs
@@ -20,25 +20,27 @@ module Database (
 import Control.Distributed.Process hiding (Message)
 import Control.Distributed.Process.Closure
 
---Generic Packages
+--Control Packages
 import Control.Concurrent hiding (newChan)
-import Control.Exception
+import Control.Exception hiding (catch)
 import Control.Monad
 import Control.Monad.IO.Class
+
+--Data Packages
 import qualified Data.Binary
+import Data.Binary hiding (get)
 import Data.Char
 import qualified Data.Map as Map
-import Data.Typeable
-import GHC.Generics (Generic)
-import Language.Haskell.TH
 import Data.Map (Map)
+import Data.Typeable
+
+--Other Packages
+import GHC.Generics (Generic)
+import Language.Haskell.TH hiding (match)
+import Prelude hiding (catch)
 import System.IO.Error hiding (catch)
 import Text.Printf
 
-import Data.Binary hiding (get)
-
-import Data.Typeable
-import GHC.Generics (Generic)
 
 --Basic Values for Keys and Values
 type Key   = String
@@ -71,12 +73,47 @@ remotable ['datastore]
 createDB :: [NodeId] -> Process Database
 createDB nodes = spawnLocal $ do
   ns <- forM nodes $ \nid -> do
-          say $ printf "spawning on %s" (show nid)
+          say $ printf "spawning process on %s" (show nid)
           spawn nid $(mkStaticClosure 'datastore)
   when (null ns) $ liftIO $ ioError (userError "no workers")
   mapM_ monitor ns
+  let 
+    workerGroups = groupWorkers ns
+    nSlices = length workerGroups
+  messageLoop workerGroups nSlices
 
+--Group workers by pairs TODO: change this
+groupWorkers :: [ProcessId] -> [[ProcessId]]
+groupWorkers [] = []
+groupWorkers (a:b:xs) = [a,b] : groupWorkers xs
+groupWorkers [x] = []
 
+{-
+The receiveWait function waits untils one of the match options in the list of
+possible outcomes becomes true, then it executes that particular action
+-}
+messageLoop :: [[ProcessId]] -> Int -> Process ()
+messageLoop workerGroups nSlices
+ = receiveWait
+        [ match $ \msg -> handleRequest msg workerGroups nSlices
+          >> messageLoop workerGroups nSlices
+        , match $ \(ProcessMonitorNotification _ pid reason) -> do
+            say (printf "process %s died: %s" (show pid) (show reason))
+            messageLoop (map (filter (/= pid)) workerGroups) nSlices
+        ]
+
+{-
+Handle a request by dispersing it out to all the processes with that
+key in their key space
+-}
+handleRequest :: Message -> [[ProcessId]] -> Int -> Process ()
+handleRequest m workerGroups nSlices =
+  case m of
+    Set k _ -> mapM_ (\x -> send x m) (keyWorkers k)
+    Get k _ -> mapM_ (\x -> send x m) (keyWorkers k)
+  where
+    keyWorkers :: Key -> [ProcessId]
+    keyWorkers k = workerGroups !! (ord (head k) `mod` nSlices)
 
 set :: Database -> Key -> Value -> Process ()
 set db k v = do
